0603470d8678a112ac0b2ea0c94feb0eb39d7926
Logan Newhall <logannewhall98@gmail.com>
2022-05-22 17:16:41 -0600

    add to cart features

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -3,7 +3,7 @@ class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', # change the user and password as needed
-                                    password = 'E2e18v94!', 
+                                    password = 'root', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
---
+++ flask_app/controllers/carts.py
@@ -0,0 +1,23 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.product import Product
+from flask_app.models.cart import Cart
+
+@app.route('/add/<product_id>')
+def add_item(product_id):
+    if 'user_id' not in session:
+        return redirect('/loginreg')
+    data = {
+        'product_id' : product_id,
+        'user_id' : session['user_id']
+    }
+    Cart.add_item(data)
+    return redirect('/view_cart')
+
+
+@app.route('/view_cart')
+def view_cart():
+    data = {
+        'user_id' : session['user_id']
+    }
+    return render_template('cart.html', cart_items = Cart.get_cart_items(data))
