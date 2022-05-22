5ca7a045da8492c8f84c91a21dee2c43d6b9ae38
Logan Newhall <logannewhall98@gmail.com>
2022-05-22 11:32:33 -0600

    added product model and controller

---
+++ flask_app/models/product.py
@@ -0,0 +1,13 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Product:
+    db='estore'
+    def __init__(self,data):
+        self.id = data['id']
+        self.title = data['title']
+        self.price = data['price']
+        self.description = data['description']
+        self.category = data['category']
+        self.image = data['image']
+        self.rating = data['rating']
+        self.user_id = data['user_id']
