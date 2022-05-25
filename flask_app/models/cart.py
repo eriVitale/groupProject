bd1134180505fd214bf66a59d7e3e54b4e99e974
Erica Vitale <erivitale@gmail.com>
2022-05-24 20:25:47 -0400

    delete fix?

--- flask_app/models/cart.py
+++ flask_app/models/cart.py
@@ -31,7 +31,6 @@ class Cart:
 
     @classmethod
     def delete(cls,data):
-        query = 'DELETE * FROM cart WHERE id =%(id)s and user_id=%(user_id)s;'
         query = 'DELETE FROM cart WHERE user_id = %(user_id)s AND product_id = %(product_id)s;'
         return connectToMySQL(cls.db).query_db(query,data)
 
